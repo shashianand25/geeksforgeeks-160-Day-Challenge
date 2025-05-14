bae2f1832e8fa17940bb986257193de11d519cf7
shashianand25 <shashianand2005@gmail.com>
2025-05-15 01:39:26 +0530

    Create Day5-next-permutation.cpp

---
+++ Day5-next-permutation.cpp
@@ -0,0 +1,22 @@
+
+// User function Template for C++
+
+class Solution {
+  public:
+
+    void nextPermutation(vector<int>& arr) {
+        int k=arr.size();
+        int i=k-2;
+        while(i>=0 && arr[i]>=arr[i+1]) {
+            i--;
+        }
+        if(i>=0) {
+            int j=k-1;
+            while(arr[j]<=arr[i]) {
+                j--;
+            }
+            swap(arr[i],arr[j]);
+        }
+        reverse(arr.begin()+i+1,arr.end());
+    }
+};
